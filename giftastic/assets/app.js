55bb0c4c51158e505150437766a1f141acc2ae7a
Blues <40874591+ABJervis@users.noreply.github.com>
2018-09-30 21:15:21 -0400

    only allow additional entry one time

--- giftastic/assets/app.js
+++ giftastic/assets/app.js
@@ -1,17 +1,21 @@
-var topics = ["trombone","bass guitar","euphonium","piano"]
+var topics = ["trombone","bass guitar","trumpet","piano"];
 
 //instrumentChosen function so the HTML will display the chosen instrument
 
-function instrumentChosen(){
+function displayInfo(){
     var instrument = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + instrument + "&api_key=dc6zaTOxFJmzC&limit=10";
+
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response){
+        //check for response
+        console.log(response)
+
         var results = response.data;
-        for (var i = 0; i< results.length; i++){
+        for (var i = 0; i < results.length; i++){
             //censor by rating//create space for the gif and text for the rating
             if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
                 var gifDiv = $("<div>");
@@ -23,32 +27,42 @@ function instrumentChosen(){
                 gifDiv.append(p);
                 gifDiv.append(instrumentImage);
                 //PREPEND gifDiv to HTML 
-                $("#music-instrument").prepend(givDiv);
+                $("#music-instrument").prepend(gifDiv);
 
             }
         }
-    })
+    });
 }
 
-//display instrument data
+//create buttons for instruments chosen
 
-function instrumentGo() {
-    $("buttons-used").empty();
-    for (var i = 0; i< topics.length; i++){
-        var a = $("<button>");
-        a.addClass("instrument-button");
-        a.attr("data-name",topics[i]);
-        a.text(topics[i]);
-        $("#buttons-used").append(a);
+function createButtons() {
+    $("#buttons-view").empty();
+    for (var i = 0; i < topics.length; i++){
+        var newButton = $("<button>");
+        newButton.addClass("instrument-btn");
+        newButton.attr("data-name",topics[i]);
+        newButton.text(topics[i]);
+        $("#buttons-view").append(newButton)
     }
-
-
-    $("#music-add").on("click", function (event){
-        event.preventDefault();
-
-        var music = $("#music-input").val().trim();
-        topics.push(music);
-
-        instrumentGo();
 }
+
+
+$("#music-add").on("click", function (event){
+    event.preventDefault();
+
+    var music = $("#music-input").val().trim();
+    topics.push(music);
+
+    console.log(topics)
+
+    createButtons();
+})
+
+
+$(document).on("click",".instrument-btn", displayInfo);
+
+
+createButtons();
+
 

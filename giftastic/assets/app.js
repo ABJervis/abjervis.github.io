18f87808c6abb3171da440181d8f17b5a46afafd
Blues <40874591+ABJervis@users.noreply.github.com>
2018-09-30 23:10:20 -0400

    need to work on static/animate

--- giftastic/assets/app.js
+++ giftastic/assets/app.js
@@ -11,22 +11,48 @@ function displayInfo(){
         url: queryURL,
         method: "GET"
     }).then(function (response){
+        
         //check for response
+        
         console.log(response)
 
         var results = response.data;
+
+        //loops over item results
+        
         for (var i = 0; i < results.length; i++){
+            
             //censor by rating//create space for the gif and text for the rating
+            
             if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
+            
+                //create a div for the gif
+            
                 var gifDiv = $("<div>");
+            
+                //stores result item's rating
+            
                 var rating = results[i].rating;
+            
+                //creates a paragraph to display rating
+            
                 var p = $("<p>").text("Rating: "+ rating);
+            
+                //image tag for the gif
+            
                 var instrumentImage = $("<img>");
+            
+                //gives image tag src attribute of a property pulled from the result item
+            
                 instrumentImage.attr("src", results[i].images.fixed_height.url);
+            
                 //need to append the paragraph, image
+            
                 gifDiv.append(p);
                 gifDiv.append(instrumentImage);
-                //PREPEND gifDiv to HTML 
+            
+                //PREPEND gifDiv to HTML so it appears first 
+            
                 $("#music-instrument").prepend(gifDiv);
 
             }
@@ -37,7 +63,9 @@ function displayInfo(){
 //create buttons for instruments chosen
 
 function createButtons() {
+    // to make sure there are not duplicate entries
     $("#buttons-view").empty();
+    
     for (var i = 0; i < topics.length; i++){
         var newButton = $("<button>");
         newButton.addClass("instrument-btn");
